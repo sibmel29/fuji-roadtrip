08baa3de0765cc3dd313e8d6cd8516d3deb83f42
Simon Berry <131280072+sibmel29@users.noreply.github.com>
2026-08-22 11:32:04 +1000

    Fix battery bridge Gradle Kotlin options

--- fuji-battery-bridge/app/build.gradle.kts
+++ fuji-battery-bridge/app/build.gradle.kts
@@ -2,5 +2,5 @@ plugins { id("com.android.application"); id("org.jetbrains.kotlin.android") }
 android {
  namespace="com.fujiroadtrip.batterybridge"; compileSdk=35
  defaultConfig { applicationId="com.fujiroadtrip.batterybridge"; minSdk=26; targetSdk=35; versionCode=1; versionName="0.1" }
+ kotlinOptions { jvmTarget="17" }
 }
-kotlinOptions { jvmTarget="17" }
