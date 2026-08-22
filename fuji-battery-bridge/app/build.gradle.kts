ddcfccf35f771e938f9aa3a35cab2d3c688e3fa3
Simon Berry <131280072+sibmel29@users.noreply.github.com>
2026-08-22 11:47:21 +1000

    Align battery bridge Java target

--- fuji-battery-bridge/app/build.gradle.kts
+++ fuji-battery-bridge/app/build.gradle.kts
@@ -1,6 +1,26 @@
-plugins { id("com.android.application"); id("org.jetbrains.kotlin.android") }
+plugins {
+    id("com.android.application")
+    id("org.jetbrains.kotlin.android")
+}
+
 android {
- namespace="com.fujiroadtrip.batterybridge"; compileSdk=35
- defaultConfig { applicationId="com.fujiroadtrip.batterybridge"; minSdk=26; targetSdk=35; versionCode=1; versionName="0.1" }
- kotlinOptions { jvmTarget="17" }
+    namespace = "com.fujiroadtrip.batterybridge"
+    compileSdk = 35
+
+    defaultConfig {
+        applicationId = "com.fujiroadtrip.batterybridge"
+        minSdk = 26
+        targetSdk = 35
+        versionCode = 1
+        versionName = "0.1"
+    }
+
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
+    }
+
+    kotlinOptions {
+        jvmTarget = "17"
+    }
 }
